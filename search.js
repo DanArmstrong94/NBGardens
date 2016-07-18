a25a5a66bef0c052fd504371bc99c6c88db74df9
Daniel Armstrong <Daniel Armstrong>
2016-07-18 16:49:55 +0100

    deleted unneccesary files, and added some scripting to the search. Still not working.

--- search.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	
-	var search = sessionStorage.getItem("searchValue");
-	if (search != "gnome"){
-		$(document).getElementById("#GnomeRow").style.display = "none";
-	}
-	
-});
