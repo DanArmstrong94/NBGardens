b2ee3abe761d3525a067195dc3b3c9c609c36e56
DanArmstrong94 <armstrongdanielc@gmail.com>
2016-07-31 22:09:26 +0100

    testing Registration JavaScript changes

--- js/app.js
+++ js/app.js
@@ -7,3 +7,26 @@ $(document).ready(function(){
       else $(this).find('span').html('&#x25BC;')
     })
 });
+
+function check(){
+	if (document.getElementById("FirstName").length < 1){
+		var firstName = document.getElementById("firstNameError");
+		firstName.style.display = 'block';
+	};
+	if (document.getElementById("Surname").length < 1){
+		var surname = document.getElementById("surnameError");
+		surname.style.display = 'block';		
+	};
+	if (document.getElementById("Email").length < 1){
+		var email = document.getElementById("emailError");
+		email.style.display = 'block';	
+	};
+	if (document.getElementById("Password").length < 8){
+		var passwordStr = document.getElementById("passwordError");
+		passwordStr.style.display = 'block';		
+	};
+	if (document.getElementById("Password2").length < 8){
+		var passwordStr2 = document.getElementById("password2Error");
+		passwordStr2.style.display = 'block';
+	};
+};
